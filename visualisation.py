f6d6d457879fc11d0a5c747704bc9104fa78588a
Tommy <tommy.schoenherr@posteo.de>
2018-12-06 09:37:40 +0100

    linked views of diagrams

--- visualisation.py
+++ visualisation.py
@@ -21,7 +21,7 @@ FILENAME = 'data.csv'
 SEPERATOR = ';'
 INTERVAL = 604800
 
-def preparePlot(name, interval):
+def preparePlot(name):
 	# Create the plot
 	plotname = "p_" + name
 	plotname = bp.figure(title="Internet "+name+" speed", width=1000, height=500, x_axis_type="datetime", x_axis_label="date/time", y_axis_label="Download speed in Mb/s")
@@ -31,9 +31,6 @@ def preparePlot(name, interval):
 	plotname.xaxis[0].formatter = DatetimeTickFormatter(days="%d.%m.%Y %H:%M:%S")
 	# add the legend
 	plotname.legend.location = "top_left"
-	# interval in seconds / unixtime; bokeh uses milliseconds
-	plotname.x_range.start = (time.time()-interval)*1000
-	plotname.x_range.end = time.time()*1000+7200
 	return plotname
 
 ### Data preparation
@@ -48,7 +45,7 @@ logger['ToolTipDates'] = logger.date_time.map(lambda x: x.strftime("%d.%m.%Y %H:
 mySource = bp.ColumnDataSource(logger)
 
 ### Create the Download plot
-p_download = preparePlot("Download", INTERVAL)
+p_download = preparePlot("Download")
 # calculating average values
 bw_dl_mean = logger['bw_dl'].mean()
 st_dl_mean = logger['st_dl'].mean()
@@ -63,13 +60,16 @@ p_download.add_tools( HoverTool(tooltips= [
 	("speedtest download:","@st_dl (avg: " + str(int(st_dl_mean)) + ") Mb/s"),
 	("other download:","@other_dl (avg: " + str(int(other_dl_mean)) + ") Mb/s")
 ]))
+# interval in seconds / unixtime; bokeh uses milliseconds
+p_download.x_range.start = (time.time()-INTERVAL)*1000
+p_download.x_range.end = time.time()*1000+7200
 
 #regression = np.polyfit(logger['date_time'].astype('int')*1000, logger['bw_dl'], 1)
 #r_x, r_y = zip(*((logger['date_time'], i*regression[0] + regression[1]) for i in range(len(logger))))
 #p_download.line(r_x, r_y, color="black")
 
 ### Create the Upload plot
-p_upload = preparePlot("Upload", INTERVAL)
+p_upload = preparePlot("Upload")
 # calculating average values
 bw_ul_mean = logger['bw_ul'].mean()
 st_ul_mean = logger['st_ul'].mean()
@@ -84,9 +84,11 @@ p_upload.add_tools( HoverTool(tooltips= [
     	("speedtest upload:","@st_ul (avg: " + str(int(st_ul_mean)) + ") Mb/s"),
 	("other upload:","@other_ul (avg: " + str(int(other_ul_mean)) + ") Mb/s")
 ]))
+p_upload.x_range = p_download.x_range
+p_upload.y_range = p_download.y_range
 
 ### Create the Internal LAN plot
-p_internal = preparePlot("Internal", INTERVAL)
+p_internal = preparePlot("Internal")
 # calculating average values
 internal_dl_mean = logger['internal_dl'].mean()
 internal_ul_mean = logger['internal_ul'].mean()
@@ -101,14 +103,18 @@ p_internal.add_tools( HoverTool(tooltips= [
 #p_internal.vbar(source=mySource,x="date_time",y="internal_ul",color="blue",size=5,legend="Internal Server to LAN bandwidth usage in Mb/s")
 p_internal.line(source=mySource,x="date_time",y="internal_dl",color="red",line_width=2,legend="Internal LAN to Server bandwidth usage in Mb/s")
 p_internal.line(source=mySource,x="date_time",y="internal_ul",color="blue",line_width=2,legend="Internal Server to LAN bandwidth usage in Mb/s")
+p_internal.x_range = p_download.x_range
+p_internal.y_range = p_download.y_range
 
 ### Create the ping plot
-p_ping = preparePlot("Ping", INTERVAL)
+p_ping = preparePlot("Ping")
 # calculate average ping
 ping_mean = logger['ping'].mean()
 # draw the line
 p_ping.vbar(source=mySource,x="date_time",top="ping",bottom=0,width=3600000,fill_color="darkgreen",line_color="darkgreen",legend="Ping in ms")
 p_ping.add_tools( HoverTool(tooltips= [("date/time:","@ToolTipDates"),("Ping:","@ping ms"),("Avg. ping:",str(ping_mean) + " ms")]))
+p_ping.x_range = p_download.x_range
+p_ping.y_range = p_download.y_range
 
 ### output to line.html
 bp.output_file("speedtest.html", title="Internet / LAN bandwidth visualisation") #put output_notebook() for notebook
